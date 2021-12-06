f98c7dae075be5e3f6b2286661894e9fc46d8892
exersalza <squidodle44@gmail.com>
2021-12-06 18:16:09 +0100

    n scheiss

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace LearnCSharp
+﻿using System;
+
+namespace LearnCSharp
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
         }
     }
 }
