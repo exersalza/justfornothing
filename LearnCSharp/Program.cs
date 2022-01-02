18182331fe7ca1b09986e00e04dc532830dcac44
exersalza <squidodle44@gmail.com>
2022-01-02 20:43:03 +0100

    timespan

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -14,18 +14,31 @@ namespace LearnCSharp
     {
         public static void Main(string[] args)
         {
-            var dateTime = new DateTime(2015, 1, 1);
-            var now = DateTime.Now;
-            var today = DateTime.Today;
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
 
-            Console.WriteLine(now.Hour);
 
-            Console.WriteLine(now.ToLongDateString());
-            Console.WriteLine(now.ToShortDateString());
-            Console.WriteLine(now.ToLongTimeString());
-            Console.WriteLine(now.ToLongTimeString());
-            Console.WriteLine(now.ToShortTimeString());
-            Console.WriteLine(now.ToShortTimeString());
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+
+            Console.WriteLine(duration);
+            
+            // Properties
+            Console.WriteLine(timeSpan.Minutes);
+            Console.WriteLine(timeSpan.TotalMinutes);
+            
+            // Add
+            Console.WriteLine("Add " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Sub " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
+            
+            // ToString
+            Console.WriteLine("ToString " + timeSpan.ToString());
+            
+            // Parse
+            Console.WriteLine("Parse " + TimeSpan.Parse("01:02:03"));
+            
         }
     }
 }
