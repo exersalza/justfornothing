e105d1695467c7b502b4afd19cb8a5b7140a07f9
exersalza <squidodle44@gmail.com>
2022-01-02 20:29:47 +0100

    More

---
+++ LearnCSharp/Chapter1/nonPrimitive.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace LearnCSharp.Chapter1
+{
+    
+}
--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -1,10 +1,31 @@
+using System;
+using System.Collections.Generic;
+
 namespace LearnCSharp
 {
+    public enum Shipping
+    {
+        Fast, 
+        Normal,
+        Slow
+    }
+    
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine(now.Hour);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToShortTimeString());
         }
     }
 }
